8850a37673d7a1d4c7cbc32fa8629de4c7db0ada
Dan Winship <danw@src.gnome.org>
2005-11-13 02:11:58 +0000

    fix some old search-and-replace-o's

--- libsoup/soup-connection-ntlm.c
+++ libsoup/soup-connection-ntlm.c
@@ -1,4 +1,4 @@
-/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-ntlm-offset: 8 -*- */
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
 /*
  * soup-connection-ntlm.c: NTLM-using Connection
  *
